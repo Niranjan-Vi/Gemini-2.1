63868b06520a0d52b3289f8938e2e3ccf55a0057
Niranjan Vi <157526421+Niranjan-Vi@users.noreply.github.com>
2024-07-09 21:45:08 +0530

    initial commit

--- script.js
+++ script.js
@@ -2,51 +2,35 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Access your API key (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI("AIzaSyACsupPzb4SFfD1HoGXxqVFwEXQua2_Rww");
-let request=document.getElementById('model');
-let text=document.getElementById('text');
-let button=document.getElementById('button');
-let input1='';
-let querries=[];
+let request1 = document.getElementById('model');
+let text = document.getElementById('text');
+let button = document.getElementById('button');
+let input1 = '';
+let querries = [];
 
-button.addEventListener('click',()=>{
-     input1=text.value;
-    querries.push(input1);
+button.addEventListener('click', () => {
+  input1 = text.value;
+  querries.push(input1, '\n');
 
+  run();
+  text.value = " ";
+  if(querries.length>=5){
+    querries.splice(0,querries.length);
+  }
 
-
-    console.log(querries);
-    run(request);
 });
 
-async function run(request) {
-  // The Gemini 1.5 models are versatile and work with multi-turn conversations (like chat)
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-  const chat = model.startChat({
-    history: [
-      {
-        role: "user",
-        parts: [{ text: querries[querries.length-1] }],
-      },
-      {
-        role: "model",
-        parts: [{ text: "Great to meet you. What would you like to know?" }],
-      },
-    ],
-    generationConfig: {
-      maxOutputTokens: 100,
-    },
-  });
-
-   
-
-  const result = await chat.sendMessage(input1);
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  let prompt = input1;
+  const result = await model.generateContent(prompt);
   const response = await result.response;
- let data = response.text();
- console.log(data);
- request.value=data;
-    
+  let text = response.text();
+  querries.push(text);
+  request1.value = querries;
+  // console.log(querries);
 }
+
 
 
 
